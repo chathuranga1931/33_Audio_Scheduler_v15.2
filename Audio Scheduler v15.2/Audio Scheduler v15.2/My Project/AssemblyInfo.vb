98c49f0006ece605888a96ce9b33d4de52377ca8
Chathuranga Dissanayaka <Chathuranga Dissanayaka>
2015-11-20 07:47:34 +0530

    2015.11.20

---
+++ Audio Scheduler v15.2/Audio Scheduler v15.2/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Audio Scheduler v15.2")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("Audio Scheduler v15.2")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("65bc50f0-6bbd-40c4-a4d3-f5aba4b09b40")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ addGIT.bat
@@ -0,0 +1,5 @@
+git init
+git add .
+git commit -m "2015.11.20"
+git remote add origin https://github.com/chathuranga1931/33_Audio_Scheduler_v15.2.git
+git push origin master
